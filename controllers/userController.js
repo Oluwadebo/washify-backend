514dbb0628c0773b8a132a865b263d0e0b35cabd
Oluwadebo <ogunweoluwadebo@gmail.com>
2025-11-07 06:23:50 +0100

    cookies change

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,8 +94,8 @@ export const loginUser = async (req, res) => {
     // ✅ Send JWT as a secure cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false, 
-      sameSite: 'lax',
+      secure: true, 
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
